714d6c4a23a2bd12d24ecd7ce27f14f911a65931
a-ori-a <ma_hiro25@outlook.jp>
2023-06-10 23:45:57 +0900

    add Updator

---
+++ widgetsUpdator.js
@@ -0,0 +1,12 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: teal; icon-glyph: magic;
+const rawFiles = ["dayCounter.js", "daysLeft.js", "randomQuotes.js","widgetsUpdator.js"]
+var fm = new FileManager()
+var root = fm.documentsDirectory()+'/'
+
+for (var file of rawFiles) {
+  var rq = new Request("https://raw.githubusercontent.com/a-ori-a/Widgets/master" + file)
+  var rawCode = await rq.loadString()
+  fm.writeString(root+file, rawCode)  // overwrite existing code with (maybe) new one
+}
