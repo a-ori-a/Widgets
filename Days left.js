6533927e7f27884fa321719f8eea162dd0da4090
a-ori-a <ma_hiro25@outlook.jp>
2023-06-10 23:17:49 +0900

    initial commit

---
+++ Days left.js
@@ -0,0 +1,51 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: deep-blue; icon-glyph: magic;
+const widget = new ListWidget()
+
+if (args.widgetParameter == null | args.widgetParameter == '') {
+  parameter = "EIKEN.2023/6/4"
+} else {
+  parameter = args.widgetParameter
+}
+var split = parameter.split('.')
+var event = split[0]
+var goal = new Date(split[1]).getTime()
+var now = new Date().getTime()
+var diffDate = Math.ceil((goal - now)/(1000 * 3600 * 24))
+if (split.length == 4) {
+	var bgColor = split[2]
+  var fgColor = split[3]
+} else {
+  var fgColor = 'dfdfdf'
+  var bgColor = '2f2f2f'
+}
+
+widget.backgroundColor = new Color(bgColor)
+
+widget.addSpacer()
+
+var event = widget.addText(event)
+event.textColor = new Color(fgColor)
+event.font = Font.boldSystemFont(14)
+event.centerAlignText()
+
+widget.addSpacer()
+
+var youhave = widget.addText('You have')
+youhave.textColor = new Color(fgColor)
+youhave.font = Font.systemFont(12)
+
+var days = widget.addText(''+diffDate)
+days.centerAlignText()
+days.textColor = new Color(fgColor)
+days.font = Font.systemFont(50)
+
+var daysleft = widget.addText('days')
+daysleft.textColor = new Color(fgColor)
+daysleft.font = Font.systemFont(12)
+daysleft.rightAlignText()
+
+widget.addSpacer()
+
+widget.presentSmall()
---
+++ randomQuotes.js
@@ -0,0 +1,100 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: deep-green; icon-glyph: magic;
+// variables declaration
+var widget = new ListWidget()
+var today = new Date()
+var fm = FileManager.local()
+var lastUpdatePath = fm.documentsDirectory()+'/lastUpdate.txt'
+var jsonPath = fm.documentsDirectory()+'/quotes.json'
+var isUpdated =true
+var fm = new FileManager.local()
+
+if (!fm.fileExists(fm.documentsDirectory()+'/quoteAPI.txt')) {
+  var alert = new alert()
+  alert.title = 'Enter API key'
+  alert.addTextField('API key')
+  alert.addAction('OK')
+  alert.addCancelAction('cancel')
+  var r = await alert.present()
+  if (r != -1) { // when api key entered
+    if (alert.textFieldValue(0).length == 32) {  // valid api key
+      fm.writeString(fm.documentsDirectory+'/quoteAPI.txt', alert.textFieldValue(0))
+    } else {
+      alert = new alert()
+      alert.title = 'Invalid API key\n try again or get API key by yourself'
+      alert.addCancelAction()
+      alert.present()
+    }
+  }
+} else {
+  var key = fm.readString(fm.documentsDirectory+'/quoteAPI.txt')
+}
+
+
+if (!fm.fileExists(lastUpdatePath)) {  // check whether this widget has run before
+  isUpdated = false
+} else {  // widget has not updated today
+  isUpdated = fm.readString(lastUpdatePath) == today.getDate()
+}
+// isUpdated = false
+if (isUpdated == false) { // when it is not updated or hasn't run before
+  var rq = new Request("https://favqs.com/api/quotes")
+  rq.headers = {
+    "Authorization": "Token token="+key
+  }
+  rq = await rq.loadString()
+  var json = JSON.parse(rq)
+  fm.writeString(lastUpdatePath, String(today.getDate()))
+  fm.writeString(jsonPath, rq)
+} else {
+  var json =JSON.parse(fm.readString(jsonPath))  // read from cache
+}
+if (args.widgetParameter == 'light') {
+  var fontColor = '232323'
+  var bgColor = 'efefef'
+} else if (args.widgetParameter == 'dark') {
+	var fontColor = 'efefef'
+  var bgColor = '232323'
+} else {
+  var fontColor = '232323'
+  var bgColor = 'efefef'
+}
+
+json = json.quotes
+
+// configure font size
+if (config.runsInAccessoryWidget) {
+  var maxLength = 70 
+  var size = 12
+  var name = false
+} else {
+  var maxLength = 100
+  var size = 18
+  var name = true
+}
+
+// choose which quote to show (length-based judgement)
+var info = ""
+while (info == "" || info.body.length >= maxLength || info.body.length <= 20) {
+  info = json[Math.floor(Math.random()*(json.length-1))]
+}
+
+// construct widget
+var quote = widget.addText(info.body)
+quote.font = new Font("Futura-MediumItalic",size)
+quote.minimumScaleFactor = 0
+
+if (name) {
+  var author = widget.addText(info.author)
+  author.font = new Font("Futura-Medium", size-7)
+  author.rightAlignText()
+  author.textColor = new Color(fontColor)
+}
+if (!config.runsInAccessoryWidget) {
+	quote.textColor = new Color(fontColor)
+	widget.backgroundColor = new Color(bgColor)
+}
+
+Script.setWidget(widget)
+widget.presentAccessoryRectangular()
