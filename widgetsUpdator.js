55e5a73c45c848eb21f1dab7335b975f701b4db2
a-ori-a <ma_hiro25@outlook.jp>
2023-06-11 00:27:48 +0900

    fix fatal bug in updator

--- widgetsUpdator.js
+++ widgetsUpdator.js
@@ -6,7 +6,7 @@ var fm = FileManager.iCloud()
 var root = fm.documentsDirectory()+'/'
 
 for (var file of rawFiles) {
-  var rq = new Request("https://raw.githubusercontent.com/a-ori-a/Widgets/master" + file)
+  var rq = new Request("https://raw.githubusercontent.com/a-ori-a/Widgets/master/" + file)
   var rawCode = await rq.loadString()
   fm.writeString(root+file, rawCode)  // overwrite existing code with (maybe) new one
 }
