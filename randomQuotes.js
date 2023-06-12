50053b4dedcdb895ecb53e82db8e113e4fe00eec
a-ori-a <96527832+a-ori-a@users.noreply.github.com>
2023-06-12 22:28:43 +0900

    randomQuotes now show error's in widgets

--- randomQuotes.js
+++ randomQuotes.js
@@ -2,7 +2,6 @@
 // These must be at the very top of the file. Do not edit.
 // icon-color: deep-green; icon-glyph: magic;
 // variables declaration
-var widget = new ListWidget()
 var today = new Date()
 var fm = FileManager.local()
 var lastUpdatePath = fm.documentsDirectory() + '/lastUpdate.txt'
@@ -11,7 +10,7 @@ var apiKeyPath = fm.documentsDirectory() + 'apiKey.txt'
 var isUpdated = true
 var purpose = 'misc'
 
-
+/// API key
 if (config.runsInApp) {
   var alert = new Alert()
   alert.title = 'Would you like to reset API key?'
@@ -23,46 +22,7 @@ if (config.runsInApp) {
   }
 }
 
-if ((!fm.fileExists(apiKeyPath)) || purpose == 'updateAPI') {
-  var alert = new Alert()
-  alert.title = 'Enter API key'
-  alert.addTextField('API key')
-  alert.addAction('OK')
-  alert.addCancelAction('Cancel')
-  var r = await alert.present()
-  if (r != -1) { // when api key entered
-    if (alert.textFieldValue(0).length == 32) {  // valid api key
-      fm.writeString(apiKeyPath, alert.textFieldValue(0))
-    } else {
-      alert = new Alert()
-      alert.title = 'Invalid API key\n try again or get API key by yourself'
-      alert.addCancelAction()
-      alert.present()
-    }
-  }
-} else {
-  var key = fm.readString(apiKeyPath)
-}
-
-
-if (!fm.fileExists(lastUpdatePath)) {  // check whether this widget has run before
-  isUpdated = false
-} else {  // widget has not updated today
-  isUpdated = fm.readString(lastUpdatePath) == today.getDate()
-}
-// isUpdated = false
-if (isUpdated == false) { // when it is not updated or hasn't run before
-  var rq = new Request("https://favqs.com/api/quotes")
-  rq.headers = {
-    "Authorization": "Token token=" + key
-  }
-  rq = await rq.loadString()
-  var json = JSON.parse(rq)
-  fm.writeString(lastUpdatePath, String(today.getDate()))
-  fm.writeString(jsonPath, rq)
-} else {
-  var json = JSON.parse(fm.readString(jsonPath))  // read from cache
-}
+// config variables
 if (args.widgetParameter == 'light') {
   var fontColor = '232323'
   var bgColor = 'efefef'
@@ -73,10 +33,6 @@ if (args.widgetParameter == 'light') {
   var fontColor = '232323'
   var bgColor = 'efefef'
 }
-
-json = json.quotes
-
-// configure font size
 if (config.runsInAccessoryWidget) {
   var maxLength = 70
   var size = 12
@@ -87,13 +43,90 @@ if (config.runsInAccessoryWidget) {
   var name = true
 }
 
+// enter API key
+if ((!fm.fileExists(apiKeyPath)) || purpose == 'updateAPI') {
+  var alert = new Alert()
+  alert.title = 'Enter API key'
+  alert.addTextField('API key')
+  alert.addAction('OK')
+  alert.addCancelAction('Cancel')
+  var r = await alert.present()
+  if (r != -1) { // when api key entered
+    if (alert.textFieldValue(0).length == 32) {  // valid api key
+      fm.writeString(apiKeyPath, alert.textFieldValue(0))
+      var key = alert.textFieldValue(0)
+      alert = new Alert()
+      alert.title = 'API key loaded successfully'
+      alert.addCancelAction('ok')
+      await alert.present()
+    } else {
+      alert = new Alert()
+      alert.title = 'Invalid API key\n try again or get API key by yourself'
+      alert.addCancelAction('ok')
+      var key = 'invalidKey'
+      await alert.present()
+    }
+  }
+} else {
+  var key = fm.readString(apiKeyPath)
+}
+
+// decide whether to load from local
+if (!fm.fileExists(lastUpdatePath)) {  // check whether this widget has run before
+  isUpdated = false
+} else {  // widget has not updated today
+  isUpdated = fm.readString(lastUpdatePath) == today.getDate()
+}
+
+try {
+  var json = await getQuotes()
+  chooseQuote()
+} catch (e) {
+  if (e.message = 'Invalid Access Token') {
+    info = {
+      'body' : 'invalid access token (API key)',
+      'author' : 'run this script from app'
+    }
+  } else {
+    isUpdated = true
+    var json = await getQuotes()  // load from local
+    chooseQuote()
+  }
+}
+
+async function getQuotes() {
+  if (isUpdated == false) { // when it is not updated or hasn't run before
+    var rq = new Request("https://favqs.com/api/quotes")
+    rq.headers = {
+      "Authorization": "Token token=" + key
+    }
+    rq = await rq.loadString()  // use loadString as string data is required later
+    if (rq == `HTTP Token: Access denied.
+`) {
+      throw new Error('Invalid Access Token')
+    }
+    var json = JSON.parse(rq)
+    fm.writeString(lastUpdatePath, String(today.getDate()))
+    fm.writeString(jsonPath, rq)
+  } else {
+    var json = JSON.parse(fm.readString(jsonPath))  // read from cache
+  }
+  return json.quotes
+}
+
 // choose which quote to show (length-based judgement)
-var info = ""
-while (info == "" || info.body.length >= maxLength || info.body.length <= 20) {
-  info = json[Math.floor(Math.random() * (json.length - 1))]
+
+function chooseQuote() {
+  info = ""
+  while (info == "" || info.body.length >= maxLength || info.body.length <= 20) {
+    info = json[Math.floor(Math.random() * (json.length - 1))]
+  }
 }
 
 // construct widget
+function createWidget() {
+}
+var widget = new ListWidget()
 var quote = widget.addText(info.body)
 quote.font = new Font("Futura-MediumItalic", size)
 quote.minimumScaleFactor = 0
@@ -110,4 +143,4 @@ if (!config.runsInAccessoryWidget) {
 }
 
 Script.setWidget(widget)
-widget.presentAccessoryRectangular()
+widget.presentMedium()
