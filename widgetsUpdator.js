24239c1f021a879450511d126590335e5083211c
a-ori-a <96527832+a-ori-a@users.noreply.github.com>
2023-07-12 11:34:05 +0900

    Update widgetsUpdator.js

--- widgetsUpdator.js
+++ widgetsUpdator.js
@@ -1,7 +1,7 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: teal; icon-glyph: magic;
-const rawFiles = ["dayCounter.js", "daysLeft.js", "randomQuotes.js", "widgetsUpdator.js", "tsfm.js"]
+const rawFiles = ["widgetsUpdator.js", "dayCounter.js", "daysLeft.js", "randomQuotes.js", "tsfm.js", "timeTable.js"]
 var fm = FileManager.iCloud()
 var root = fm.documentsDirectory() + '/'
 var anyUpdate = false
