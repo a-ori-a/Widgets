6b1d01060d96670e92a210d7ba6d685bf2ea386b
a-ori-a <ma_hiro25@outlook.jp>
2023-08-08 20:33:16 +0900

    declare icon size and color

--- wultidly.js
+++ wultidly.js
@@ -1,3 +1,6 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: orange; icon-glyph: stopwatch;
 var code = ''
 var widget = new ListWidget()
 var fm = FileManager.local()
