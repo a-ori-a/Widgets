46160a86263dc2bd7c6c2484dc32d5b34b15620d
a-ori-a <96527832+a-ori-a@users.noreply.github.com>
2023-09-11 06:28:33 +0900

    Update icon

--- Reminder.js
+++ Reminder.js
@@ -1,6 +1,6 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
-// icon-color: orange; icon-glyph: magic;
+// icon-color: red; icon-glyph: list-ul;
 var cd = await Calendar.forReminders()
 var widget = new ListWidget()
 var today = new Date()
