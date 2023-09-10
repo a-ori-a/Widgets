ce995272a01e36910d0a55ef3ce532863c35bf3b
a-ori-a <96527832+a-ori-a@users.noreply.github.com>
2023-09-11 06:34:30 +0900

    Better updater

--- widgetsUpdater.js
+++ widgetsUpdater.js
@@ -1,11 +1,21 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
-// icon-color: teal; icon-glyph: magic;
-const rawFiles = ["widgetsUpdater.js", "dayCounter.js", "randomQuotes.js", "tsfm.js", "timeTable.js", "wultidly.js"]
+// icon-color: teal; icon-glyph: sync-alt;
+const rawFiles = ["dayCounter.js", "randomQuotes.js", "tsfm.js", "timeTable.js", "wultidly.js", "Calendar.js", "Reminder.js", "Weather.js"]
 var fm = FileManager.iCloud()
 var root = fm.documentsDirectory() + '/'
 var anyUpdate = false
 
+var rq = new Request('https://raw.githubusercontent.com/a-ori-a/Widgets/master/widgetsUpdater.js')
+var updater = await rq.loadString()
+if (updater != fm.readString(root+'widgetsUpdater.js')) {
+  fm.writeString(root+'widgetsUpdater.js', updater)
+  var al = new Alert()
+  al.title = 'Update found on widgetsUpdater'
+  al.message = 'run this script again'
+  al.addAction('OK')
+  al.present()
+}
 for (var file of rawFiles) {
   var rq = new Request("https://raw.githubusercontent.com/a-ori-a/Widgets/master/" + file)
   var rawCode = await rq.loadString()
@@ -14,7 +24,7 @@ for (var file of rawFiles) {
       alt = new Alert()
       alt.addAction("Update")
       alt.addCancelAction("Don't update plz")
-      alt.title = "Update found in " + file
+      alt.title = "Update found on " + file
       var update = await alt.present()
       if (update == -1) {
       } else {
@@ -29,12 +39,13 @@ for (var file of rawFiles) {
 
 if (anyUpdate) {
   alt = new Alert()
-  alt.title = "Script updated"
+  alt.title = "Update complete"
   alt.addAction("ok")
   await alt.present()
 } else {
   var alt = new Alert()
-  alt.title = "No script was updated"
+  alt.title = "No update found"
+  alt.message = "Your scripta are up to date"
   alt.addAction("ok")
   await alt.present()
 }
