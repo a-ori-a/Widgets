87af7a8bf99f491e689e29136e246ae006f81e9a
a-ori-a <96527832+a-ori-a@users.noreply.github.com>
2023-09-11 06:29:17 +0900

    Update icon

--- dayCounter.js
+++ dayCounter.js
@@ -1,6 +1,6 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
-// icon-color: deep-blue; icon-glyph: magic;
+// icon-color: brown; icon-glyph: business-time;
 const base = new ListWidget()
 
 var sunday = false
